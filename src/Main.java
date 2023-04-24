f773ab54f78c16c5a73a62a7ac143238c8b70a9e
Beka Iarajuli <b.iarajuli@oppa.io>
2023-04-24 15:25:24 +0400

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.security.SecureRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(randomPasswordGenerator(10));
+    }
+    public static String randomPasswordGenerator(int passwordSize) {
+        String passwordGenerator = "abcdefghijklmNBSpqrstuvwxyzALOMBSGQIODAOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]\\\\|;:'\\\"<>,.?/\"";
+        SecureRandom rnd = new SecureRandom();
+        char[] password = new char[passwordSize];
+        for (int i = 0; i < passwordSize; i++) {
+            password[i] = passwordGenerator.charAt(rnd.nextInt(passwordGenerator.length()-1));
+        }
+        return new String(password);
+    }
+}
